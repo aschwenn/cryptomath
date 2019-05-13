a884a894a927e0a40b6c4a8f0554edbc070fc0a9
Cody Cook <codycook96@gmail.com>
2019-05-13 17:48:16 +0000

    Adding __init__.py for EllipticCurve

--- EllipticCurve/EllipticCurve.py
+++ EllipticCurve/EllipticCurve.py
@@ -1,18 +1,23 @@
 #Elliptic Curve objects with corresponding functions
 
+from Algorithms
+
 #Point Object
 class Point
-    def __init__(self, x, y)
+    def __init__(self, x, y, inf = False)
         self.__x = x
         self.__y = y
+        self.__inf = inf
 
     def set(self, otherPoint)
         self.__x = otherPoint.__x
         self.__y = otherPoint.__y
+        self.__inf = otherPoint.__inf
 
-    def set(self, x, y)
+    def set(self, x, y, inf = False)
         self.__x = x
         self.__y = y
+        self.__inf = inf
 
     def getX(self)
         return self.__x
@@ -20,6 +25,8 @@ class Point
     def getY(self)
         return self.__y
 
+    def getInf(inf)
+        return self.__inf
 
     def __add__(self, otherPoint)
         return Point(self.__x + otherPoint.x, self.__y + otherPoint.y)
@@ -35,17 +42,25 @@ class Point
 
 #Elliptic Curve of form y^2 = x^3 + Ax + B
 class EllipticCurve
-    def __init__(self, A, B)
+    def __init__(self, A, B, m = 0)
         if (4*(A**3) + 27*(B**2)) = 0
             raise ValueError ('Error: 4A^3 + 27B^2 = 0 (Curve isn\'t non-singular)')
         self.__A = A
         self.__B = B
 
     def addPoint(self, firstPoint, secondPoint)
-        if firstPoint.x == secondPoint.y
+        if firstPoint.x == secondPoint.x
             if firstPoint.y == -secondPoint.y
+                return Point(0, 0, True)
+            else if firstPoint.y == secondPoint.y
+                slopeNum = (3*(firstPoint.x**2) + self.A)
+                if m == 0 #Non-Modular Curve
+                    slopeDen = 
+                else #Modular Curve
 
-            else
+
+
+
 
     def doublePoint(self, firstPoint)
         return self.addPoint(firstPoint, firstPoint)

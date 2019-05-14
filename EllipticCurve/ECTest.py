de071b6050ae568c461ea5e91207543c5af7e2c1
Cody Cook <codycook96@gmail.com>
2019-05-14 02:22:18 +0000

    Temporary testing script

---
+++ EllipticCurve/ECTest.py
@@ -0,0 +1,10 @@
+from EllipticCurve import *
+
+A = Point(1, 1)
+B = Point(2, 5)
+C = A + B
+
+print("A: (", A.x, ", ", A.y, ")")
+print("B: (", B.x, ", ", B.y, ")")
+print("C: (", C.x, ", ", C.y, ")")
+
